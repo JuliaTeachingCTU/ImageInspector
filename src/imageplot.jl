b1e2b77a3f4b3da38d04a100fa350009c0c37627
VaclavMacha <vaclav.macha92@gmail.com>
2021-03-26 14:10:06 +0100

    Improved imageplot function

--- src/imageplot.jl
+++ src/imageplot.jl
@@ -3,10 +3,21 @@ using .Plots
 export imageplot
 
 function imageplot(x, ind; flip = true, nrows = -1, ncols = -1, sep = 1, kwargs...)
-    return plot(imagegrid(x, ind; flip, nrows, ncols, sep);
-        legend = false,
-        axis = nothing,
-        border = :none,
-        kwargs...
-    )
+    img = imagegrid(x, ind; flip, nrows, ncols, sep)
+    return imageplot(img; kwargs...)
+end
+
+function imageplot(x; flip = true, kwargs...)
+    img = image(x; flip)
+    return imageplot(img; kwargs...)
+end
+
+function imageplot(
+    x::AbstractMatrix{<:Color};
+    legend = false,
+    axis = nothing,
+    border = :none,
+    kwargs...
+)
+    return plot(x; legend, axis, border, kwargs...)
 end
