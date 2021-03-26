125fc6aff611c1fc87ef3c923e76b7f37b5072a3
VaclavMacha <vaclav.macha92@gmail.com>
2021-03-26 09:12:39 +0100

    Improved image function

--- scripts/example.jl
+++ scripts/example.jl
@@ -2,4 +2,9 @@ using Revise # importantly, this must come before `using ImageInspector`
 using ImageInspector, MLDatasets, Plots
 
 x = MNIST.traintensor(1);
-plot(image(x); axis = nothing, border = :none)
+plot(
+    plot(image(x; flip = true); title = "flip = true"),
+    plot(image(x; flip = false); title = "flip = false");
+    axis = nothing,
+    border = :none,
+)
--- src/ImageInspector.jl
+++ src/ImageInspector.jl
@@ -4,6 +4,34 @@ using Colors
 
 export image
 
-image(x::AbstractMatrix{<:Real}) = Gray.(x)
+"""
+    image(x::AbstractMatrix{T}; flip = true)
+
+Converts a matrix of real numbers to the matrix of `Gray` points. If the keyword argument
+`flip` is true, the resulting matrix is transposed.
+
+# Example
+
+```julia-repl
+julia> x = [0.1 0.25; 0.4 0.6]
+2×2 Array{Float64,2}:
+ 0.1  0.25
+ 0.4  0.6
+
+julia> image(x)
+2×2 Array{Gray{Float64},2} with eltype Gray{Float64}:
+ Gray{Float64}(0.1)   Gray{Float64}(0.4)
+ Gray{Float64}(0.25)  Gray{Float64}(0.6)
+
+julia> image(x; flip = false)
+2×2 Array{Gray{Float64},2} with eltype Gray{Float64}:
+ Gray{Float64}(0.1)  Gray{Float64}(0.25)
+ Gray{Float64}(0.4)  Gray{Float64}(0.6)
+```
+"""
+function image(x::AbstractMatrix{T}; flip = true) where {T <: Real}
+    xx = flip ? PermutedDimsArray(x, (2, 1)) : x
+    return Gray.(xx)
+end
 
 end
